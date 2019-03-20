6538b47125615420365c12783d23c726d7112222
davis67 <davis67@gmail.com>
2019-03-20 18:54:14 +0300

    Added the template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import AddIssue from './components/issues/addIssue';
+import IssueDetail from './components/issues/issueDetails';
+import IssuesList from './components/issues/issuesList';
+import EditIssue from './components/issues/editIssue';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hrms Issue Tracker</h1>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <div>
+            <Switch>
+              <Route exact path="/" Component={IssuesList} />
+              <Route exact path="/add" Component={AddIssue} />
+              <Route exact path="/issue/:id" Component={IssueDetail} />;
+              <Route exact path="/edit/:id" Component={EditIssue} />;
+
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
